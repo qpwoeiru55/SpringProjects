fc250b2b44fe5ae59fc754cb4fbd5bb78fdfbb3f
DESKTOP-1EDMQ3M\82104 <wlstjrwlstjr18@gmail.com>
2025-08-20 13:17:04 +0900

    AOP

---
+++ aop/src/test/java/hello/aop/AopTest.java
@@ -0,0 +1,41 @@
+package hello.aop;
+
+import hello.aop.order.OrderRepository;
+import hello.aop.order.OrderService;
+import hello.aop.order.aop.AspectV1;
+import hello.aop.order.aop.AspectV2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.support.AopUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+@Import(AspectV2.class) //추가
+@SpringBootTest
+public class AopTest {
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Test
+    void aopInfo() {
+        log.info("isAopProxy, orderService={}", AopUtils.isAopProxy(orderService));
+        log.info("isAopProxy, orderRepository={}", AopUtils.isAopProxy(orderRepository));
+    }
+
+    @Test
+    void success() {
+        orderService.orderItem("itemA");
+    }
+
+    @Test
+    void exception() {
+        assertThatThrownBy(() -> orderService.orderItem("ex")).isInstanceOf(IllegalStateException.class);
+    }
+}
